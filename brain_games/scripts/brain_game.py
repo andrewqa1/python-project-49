7fd7917ad5f1eb18518021eeeec6e19d2fc1443f
GLADCHENKO Andrey O <Andrey.GLADCHENKO@raiffeisen.ru>
2024-07-23 13:11:05 +0400

    test

--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -9,6 +9,7 @@ def main():
     print(Fore.GREEN + 'Welcome to the Brain Games!' + Fore.RESET)
     name = prompt.string(Fore.YELLOW + 'May I have your name? ' + Fore.RESET)
     print(Fore.GREEN + f'Hello, {name}!' + Fore.RESET)
+    return name
 
 
 if __name__ == '__main__':
